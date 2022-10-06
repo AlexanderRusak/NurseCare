af559b2a9b2e372758e3892f7689292cfdb2aa3b
Александр Русак <aleksandrrusak@Mac-mini-Aleksandr.local>
2022-10-06 15:02:57 +0600

    add location almaty location

--- client/screens/GeolocationScreen.tsx
+++ client/screens/GeolocationScreen.tsx
@@ -1,24 +1,58 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import YaMap from 'react-native-yamap';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import YaMap, {Geocoder} from 'react-native-yamap';
 import {StackNavigation} from '../interfaces/StackNavigation';
 import {HOMESCREEN} from './ScreenNames';
+import Geolocation, {GeoPosition} from 'react-native-geolocation-service';
+import {getPermission} from '../map/permission';
 
 export const GeolocationScreen = () => {
   const {navigate} = useNavigation<StackNavigation>();
+
+  const [currentPosition, setCurrentPosition] = useState<GeoPosition | null>(
+    null,
+  );
+  const [hasPermission, setPermission] = useState(false);
 
   const backToMainScreenHandler = () => {
     navigate(HOMESCREEN);
   };
 
+  const hasPermissionAsync = useCallback(async () => {
+    const hasPermission = await getPermission();
+    setPermission(hasPermission);
+  }, []);
+
+  useEffect(() => {
+    hasPermissionAsync();
+    hasPermission &&
+      Geolocation.getCurrentPosition(
+        position => {
+          setCurrentPosition(position);
+        },
+        err => {
+          console.log(err);
+        },
+        { enableHighAccuracy: false, timeout: 15000, maximumAge: 10000 }
+      );
+  }, [hasPermissionAsync, hasPermission]);
+
+  console.log(currentPosition, 'sss');
+
   return (
     <View style={styles.container}>
       <Text>Geolactaion</Text>
-      <YaMap
-        showUserPosition
-        style={{flex: 1, width:'100%',height:'100%'}}
-      />
+      {currentPosition ? (
+        <YaMap
+          initialRegion={{
+            lat: currentPosition.coords.latitude,
+            lon: currentPosition.coords.longitude,
+          }}
+          showUserPosition
+          style={{flex: 1, width: '100%', height: '100%'}}
+        />
+      ) : null}
       <Button onPress={backToMainScreenHandler} title="Back" />
     </View>
   );
